fd5d04ddad1e8646f0ef2888338652fefe7526fa
Haileamlak Sahle <haileamlaksahle@gmail.com>
2024-03-19 19:09:54 +0300

    Added Challenge2 and Challenge3 files

---
+++ Challenge2.py
@@ -0,0 +1,12 @@
+""" Challenge 2
+
+Task
+The provided code stub reads and integer, n, 
+from STDIN. For all non-negative integers i<n, print i**2. """
+
+# The Code
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(5):
+    print(i**2)
